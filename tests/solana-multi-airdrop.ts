32fadbd76f97d712d17ca98cb9c8e9d1d09ca577
LedgerNinja0x <sweetheart199558@gmail.com>
2024-06-03 04:49:28 -0400

    feat:deploy devnet

--- tests/solana-multi-airdrop.ts
+++ tests/solana-multi-airdrop.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaMultiAirdrop } from "../target/types/solana_multi_airdrop";
 import { TOKEN_PROGRAM_ID, createMint, getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
-import { PublicKey, Keypair } from "@solana/web3.js";
+import { Keypair } from "@solana/web3.js";
 
 describe("solana-multi-airdrop", () => {
   // Configure the client to use the local cluster.
